2b064b03d41a91452658966d7455e4abf5fa5a28
Ritik <ritik151112@gmail.com>
2023-08-20 17:08:44 +0530

    FirstCommit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = "mysql:host=localhost;dbname=u356562325_reliso_db";
+$username = "u356562325_reliso_natura";
+$password = "hEUq4;3|F";
+
+$conn = new PDO($db_name, $username, $password);
+
+?>
